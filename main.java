702203139631d916d90dda51f6104926db380090
kanra1403 <c0116091af@edu.teu.ac.jp>
2018-11-09 15:15:12 +0900

    敗北勝利フラグ追加

--- main.java
+++ main.java
@@ -1,5 +1,13 @@
 class main{
+	
+	//勝利条件を満たしているか
+	public boolean vic_con = false;
+	//敗北条件を満たしているか
+	public boolean def_con = false;
+
 	public static void main(String[] args) {
-		
+		//5*5の部屋のダンジョンを作成
+		int[] dangeon = new int[5,5];
+		Player player = new Player();
 	}
 }
