a2d2a7376f06ef290eac03c3c31b47952450675b
kanra1403 <c0116091af@edu.teu.ac.jp>
2018-11-09 16:10:18 +0900

    移動用メソッドを追加

--- main.java
+++ main.java
@@ -8,6 +8,32 @@ class main{
 	public static void main(String[] args) {
 		//5*5の部屋のダンジョンを作成
 		int[] dangeon = new int[5,5];
+		//プレイヤーを作成
 		Player player = new Player();
+
 	}
+	
+	//プレイヤーの移動
+	//神崎
+	public int[] move(String direction){
+		//現在地を取得
+		int[] mp = getMYPoint();
+		//入力された値を大文字に変換
+		String d = direction.toUpperCase;
+		//1マス移動
+		//W:上(北) / S:下(南) / A:左(西) / D:右(東)
+		if (d.equals('W')) {
+			mp[1] -= 1;
+		}else if (d.equals('S')) {
+			mp[1] += 1;
+		}else if (d.equals('A')) {
+			mp[0] -= 1;
+		}else if (d.equals('D')) {
+			mp[0] += 1;
+		}else{
+			System.out.println("WASDで1文字入力してください");
+		}
+		player.setMyPoint(mp);
+	}
+	
 }
