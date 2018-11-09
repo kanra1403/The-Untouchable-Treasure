d764ef9f82cc7b09052a55fe1f23cf6def59b1f4
c0116247 <44888701+c0116247@users.noreply.github.com>
2018-11-09 15:03:10 +0900

    Update main.java

--- main.java
+++ main.java
@@ -2,4 +2,12 @@ class main{
 	public static void main(String[] args) {
 		
 	}
+	
+	public void Trap(boolean poison){//トラップのメソッド。ポイズントラップであれば毒状態に、ポイズントラップでなければ2ダメージ。松田
+		if(poison){
+			
+		}else{
+			
+		}
+		
 }
