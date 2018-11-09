ebdc8b6585956e1ee871aed48671018a2907bd64
kamekichi1990 <34013879+kamekichi1990@users.noreply.github.com>
2018-11-09 15:32:23 +0900

    毒ダメージメソット追加
    毒ダメージメソットと変数のコメントを追加

--- Player.java
+++ Player.java
@@ -1,16 +1,31 @@
-package TUT;
-
 class Player {
-	public Player(){
+	public static Player(){
+		//プレイヤーのステータス
 		int Hp = 10;
 		boolean Poison = false;
+		int Poisonturn = 0;
 		
+		//カード枚数
+		int BattleCard = 0;
+		int SwordCard　= 0;
+		int ShieldCard = 0;
+		int MagicCard = 0;
+		
+		//プレーヤーの場所
 		int[] MyPoint;
 		
-		boolean password = false;
+		//モーションフラグ
 		boolean MoveFrag = false;
 		boolean BattleFrag = false;
+		
+		//アイテムフラグ
 		boolean Koukaton = false;
 		boolean Yakushin = false;
+		boolean password = false;
+		
+	}
+	//毒ダメージ関数
+	public Poison(){
+		Player.Hp--;
 	}
 }
