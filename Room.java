0dbcffc695d521ab736049313bbb8e7af115c54c
TakePan <44888546+TakeutiPanda@users.noreply.github.com>
2018-11-09 15:28:17 +0900

    Update Room.java
    部屋のメソッド(空)

--- Room.java
+++ Room.java
@@ -1,9 +1,29 @@
 class Room {
 	public Room(){
-		
+		// チュートリアル部屋
 		public void TutorialRoom(){
 		
 		}
-		
+                //空部屋
+                public void EmptyRoom(){
+                    
+                }
+                //敵部屋
+                public void EnemyRoom(){
+                    
+                }
+                //宝部屋
+                public void TresureRoom(){
+                    
+                }
+                //罠部屋
+                public void TrapRoom(){
+                    
+                }
+                //秘宝
+		public void GoolRoom(){
+                    
+                }
+
 	}
 }
